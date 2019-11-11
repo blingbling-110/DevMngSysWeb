0fba2dea16dc20eeb1b045e8cd5708cd5ecd4a8b
qinzijun <qinzijun@dias.com.cn>
2019-11-11 17:11:00 +0800

    first commit

---
+++ src/test/java/com/qzj/devmngsys/DevMngSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qzj.devmngsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevMngSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
